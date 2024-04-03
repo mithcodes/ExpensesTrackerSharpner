6ef35dafbeed5754227366d670c8a4249591fcca
mithcodes <singhmithlesh791@gmail.com>
2024-04-04 03:11:22 +0530

    update

--- 04/src/pages/Github.jsx
+++ 04/src/pages/Github.jsx
@@ -1,9 +1,35 @@
-const Github=()=>{
-    return(
+const Github = () => {
+    return (
         <div className="github">
-            <h1>github</h1>
+            <div className="heading">
+                <div className="row">
+                    <div className="col-md-6">
+                        <h6>winners never quites, quitters never win</h6>
+                    </div>
+                    <div className="col-md-6">
+                        <h6>your profile is 64% completed. Complete your profile, it will increase your chances of getting a job</h6>
+                    </div>
+                </div>
+                <hr />
+            </div>
+            <div className="form">
+                <h4>Contact detail</h4>
+                <div className="input">
+                    <div className="row m-2">
+                        <div className="col-md-6 ">
+                            Full Name:
+                            <input type="text" />
+                        </div>
+                        <div className="col-md-6 ">
+                            Profile photo url:
+                            <input type="file" />
+                        </div>
+                    </div>
+                    <button className="btn btn-success m-3" type="button">Update Profile</button>  {/* Corrected button type and class */}
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Github
+export default Github;
